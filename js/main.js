77ad492ed3c98b3f9aea1ef1c71a627a075d81ad
Graphic <graphic@SOD-SJH-605-05M.local>
2016-02-02 16:02:57 -0500

    switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 
+$ ('button').on('click', function () {
+
+	$('button').toggleClass('off');
+		
+	if($('button').hasClass('off')) {
+		$('body').toggleClass('dark');
+		$('.status').text('Hey, who turn off the lights?');
+	}
+
+	else { $('body').removeClass('dark');
+		$('.status').text('Its so bright in here!');
+}
+	
+});
 
